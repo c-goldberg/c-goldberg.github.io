d53b8de2df437dcf0105fd5a28c184c00b6a8068
User <user@vagelos-ve735-0600.apnd.wlan.private.upenn.edu>
2018-06-05 14:54:39 -0400

    Not even sure what I did.

---
+++ python/address.py
@@ -0,0 +1,5 @@
+address = {"name": "Charlie Goldberg", "street": "3277 Pheasant Run NE", "city": "Minneapolis", "zip": "55449"}
+
+print(address[name])
+print(address[street])
+print(address[city] + "," + address[state] + "," + address [zip])
