752408b1aeb7290c54c627bef3d72bbe88d744cf
User <user@vagelos-ve735-0600.apnd.wlan.private.upenn.edu>
2018-06-06 10:28:21 -0400

    Made a file specifying when authors died.

---
+++ python/authors.py
@@ -0,0 +1,9 @@
+Authors = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889",
+}
+
+for author, year in Authors.items():
+    print(year + " is when " + author + " kicked the bucket.")
